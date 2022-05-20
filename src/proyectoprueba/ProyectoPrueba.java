d51f2773794776e49425ea49190df52e578d6da0
Fernando Elotlan <zS20020852@estudiantes.uv.mx>
2022-05-20 09:50:44 -0500

    Agregamos un cambio

--- src/proyectoprueba/ProyectoPrueba.java
+++ src/proyectoprueba/ProyectoPrueba.java
@@ -31,7 +31,7 @@ public class ProyectoPrueba extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        System.out.println("Prueba de cambios");
     }
     
 }
